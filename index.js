333d419a66c596484849f0d93e659ac89327f372
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-06-16 13:28:46 +0530

    done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
             "http:localhost:3000",
             "http://localhost:3000",
             "http:localhost:5173",
-            "https://railway-mern-production.up.railway.app"
+            "https://e-commerce-rho-hazel.vercel.app/"
         ]
         if(allowed.indexOf(o) !== -1 || !o){
             cb(null, true)
